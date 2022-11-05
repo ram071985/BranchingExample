ed4a391f11f9468456daa82ab9d6130a48f5b5ed
ram071985 <reidmuchow@gmail.com>
2022-11-05 17:53:29 -0500

    added scratch.js

---
+++ scratch.js
@@ -0,0 +1,39 @@
+//// Creating a Working Branch On Git ////
+
+// 1) From the main branch create a new working branch
+// git checkout -b working-branch-name
+
+// 2) Add changes and make commit
+// git add . 
+// git commit -m"changes"
+
+// 3) Push changes to new working branch
+// git push origin working-branch-name
+
+
+//// Merging Changes From Working Branch With Your Main Branch ////
+
+// 1) Checkout of working branch into main branch
+// git checkout main
+
+// 2) Merge changes from working branch into main or production branch
+// git merge working-branch-name
+
+// Push merged changes from working brnach into remote repository
+// git push origin main
+
+
+//// VSC Shortcuts ////
+
+// 1) HTML Boilerplate
+// !
+
+// 2) HTML linter
+// Windows - Shift + Alt + F
+// Mac - Shift + Option + F
+
+// 3) Save file
+// cmnd + s 
+// 4) Save all changes 
+// cmnd + shift + s
+
